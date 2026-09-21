641d076a5a031fa2df89cda4200ab369d1f34b8a
ACLL <120932801+3POPSTA@users.noreply.github.com>
2026-09-21 11:45:15 +0100

    Nodejs Imports & Exports

---
+++ Modules/exports.js
@@ -0,0 +1,17 @@
+//Exporting codes
+
+//Traditional way of exporting
+// const printDate = function (){
+//     console.log("21/09/2026")
+// }
+
+// module.exports = printDate;
+
+
+
+//The Modern Way of expoting
+export const date = "21/09/2026";
+
+export function printDate(){
+    console.log("21/09/23");
+}
