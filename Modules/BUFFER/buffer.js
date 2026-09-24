caebaa62cc687a50cbf4f2da04bf8368524e5efa
ACLL <120932801+3POPSTA@users.noreply.github.com>
2026-09-24 13:01:22 +0100

    Node Buffer Module

---
+++ Modules/BUFFER/buffer.js
@@ -0,0 +1,23 @@
+//* Bacis Buffer
+// const buf = Buffer.from("Hello, Node.js!");
+
+// console.log(buf);
+// console.log(buf.toString());
+// console.log(buf[0]); // 72 (ASCII for 'H')
+// console.log(buf.length);
+
+
+//* Buffer.alloc()
+// Create a buffer of 10 bytes filled with zeros
+// const buffer = Buffer.alloc(10);
+// console.log(buffer)
+
+
+//* Buffer.allocUnsafe()
+// const buffer2 = Buffer.allocUnsafe(10);
+// console.log(buffer2);
+
+// // Fill the buffer with zeros for security
+// buffer2.fill(1);
+// console.log(buffer2);
+
